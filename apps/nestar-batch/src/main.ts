14cc221b22d56a908a4c8fabdf7ce4b622f7c063
IsmoilovMax <maxmudjon2121@gmail.com>
2024-08-15 01:20:31 +0900

    feat: Congigure env

--- apps/nestar-batch/src/main.ts
+++ apps/nestar-batch/src/main.ts
@@ -3,6 +3,6 @@ import { NestarBatchModule } from './nestar-batch.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(NestarBatchModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT_BATCH ?? 3000);
 }
 bootstrap();
--- apps/nestar-batch/src/nestar-batch.module.ts
+++ apps/nestar-batch/src/nestar-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NestarBatchController } from './nestar-batch.controller';
 import { NestarBatchService } from './nestar-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [NestarBatchController],
   providers: [NestarBatchService],
 })
