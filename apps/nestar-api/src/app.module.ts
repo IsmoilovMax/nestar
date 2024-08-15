52b700de78de1687f92ad501ccbee62552053bb7
IsmoilovMax <maxmudjon2121@gmail.com>
2024-08-16 00:32:53 +0900

    feat: Integration GraphQLModule and Build First GraphQl API

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {ConfigModule} from "@nestjs/config"
+import { GraphQLModule } from "@nestjs/graphql"
+import { ApolloDriver } from '@nestjs/apollo';
+import { AppResolver } from './app.resolver';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: 
+  [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      playground: true,
+      uploads: false,
+      autoSchemaFile: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
---
+++ apps/nestar-api/src/app.resolver.ts
@@ -0,0 +1,11 @@
+import { Query, Resolver } from "@nestjs/graphql";
+
+@Resolver()
+export class AppResolver {
+    @Query(() => String)
+    public sayHello(): string {
+        return "GraphQL API Server";
+    }
+    
+}
+
