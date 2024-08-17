a1e02e47ffa78c126d95b069e94dc48f24a8a4b9
IsmoilovMax <maxmudjon2121@gmail.com>
2024-08-18 05:51:23 +0900

    feat: Create member schema

--- apps/nestar-api/src/components/member/member.module.ts
+++ apps/nestar-api/src/components/member/member.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MemberResolver } from './member.resolver';
 import { MemberService } from './member.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import MemberSchema from '../../schemas/Member.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{name: "Member", schema: MemberSchema}])
+  ],
   providers: [MemberResolver, MemberService]
 })
 export class MemberModule {}
