ae747e1077d8d6e6286a20a1db4d0524049a018a
IsmoilovMax <maxmudjon2121@gmail.com>
2024-08-25 05:41:30 +0900

    feat: Integration of Authenticaion and Authorization

--- apps/nestar-api/src/components/auth/auth.service.ts
+++ apps/nestar-api/src/components/auth/auth.service.ts
@@ -3,6 +3,7 @@ import * as bcrypt from "bcryptjs"
 import { Member } from '../../libs/dto/member/member';
 import { T } from '../../libs/types/common';
 import { JwtService } from '@nestjs/jwt';
+import { shapeIntoMongoObjectId } from '../../libs/types/config';
 
 
 @Injectable()
@@ -32,6 +33,8 @@ export class AuthService {
 
     public async verifyToken(token: string): Promise<Member> {
         const member = await this.jwtService.verifyAsync(token);
+        member._id = shapeIntoMongoObjectId(member._id)
+
         return member;
     } 
 }
--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,8 +1,11 @@
 import { Mutation, Resolver, Query, Args } from '@nestjs/graphql';
 import { MemberService } from './member.service';
-import { InternalServerErrorException} from '@nestjs/common';
+import { InternalServerErrorException, UseGuards} from '@nestjs/common';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { Member } from '../../libs/dto/member/member';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { ObjectId } from 'mongoose';
 
 
 
@@ -23,10 +26,19 @@ export class MemberResolver {
     }
 
     //Authenticeted
+    @UseGuards(AuthGuard)
     @Mutation(() => String)
-    public async updateMember(): Promise<string> {
+    public async updateMember(@AuthMember("_id") memberId: ObjectId): Promise<string> {
         console.log("Mutation: updateMember");
         return this.memberService.updateMember();
+    }
+
+    @UseGuards(AuthGuard)
+    @Query(() => String)
+    public async checkAuth(@AuthMember("memberNick") memberNick: string): Promise<string> {
+        console.log("Query: checkAuth");
+        console.log("memberNick: memberNick");
+        return `Hi ${memberNick}`;
     }
  
     @Query(() => String)
