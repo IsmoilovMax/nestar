f568c7f47fc877b3ed487d87e9c7d7ed164783fe
IsmoilovMax <maxmudjon2121@gmail.com>
2024-09-10 23:53:14 +0900

    feat: Integration of lookupAuthMemberFollowed complex Query logic

--- apps/nestar-api/src/components/follow/follow.service.ts
+++ apps/nestar-api/src/components/follow/follow.service.ts
@@ -6,7 +6,7 @@ import { Model, ObjectId } from 'mongoose';
 import { Direction, Message } from '../../libs/enums/common.enum';
 import { FollowInquiry } from '../../libs/dto/follow/follow.input';
 import { T } from '../../libs/types/common';
-import { lookupAuthMemberLiked, lookupFollowerData, lookupFollowingData } from '../../libs/config';
+import { lookupAuthMemberFollowed, lookupAuthMemberLiked, lookupFollowerData, lookupFollowingData } from '../../libs/config';
 
 @Injectable()
 export class FollowService {
@@ -77,6 +77,7 @@ export class FollowService {
 							//meLiked
 							lookupAuthMemberLiked(memberId, "$followingId"),
 							//meFollowed
+							lookupAuthMemberFollowed({ followerId: memberId, followingId: '$followingId'}),
 							lookupFollowingData,
 							{ $unwind: '$followingData' },
 						],
@@ -108,6 +109,7 @@ export class FollowService {
 							{ $limit: limit },
 							lookupAuthMemberLiked(memberId, "$followerId"),
 							//meFollowed
+							lookupAuthMemberFollowed({ followerId: memberId, followingId: '$followerId'}),
 							lookupFollowerData,
 							{ $unwind: '$followerData' },
 						],
--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -59,6 +59,44 @@ export const lookupAuthMemberLiked = (memberId: T, targetRefId: string = '$_id')
 	};
 };
 
+interface LookupAuthMemberFollowed {
+	followerId: T;
+	followingId: string;
+}
+
+export const lookupAuthMemberFollowed = (input: LookupAuthMemberFollowed) => {
+	const { followerId, followingId } = input;
+	return {
+		$lookup: {
+			from: 'follows',
+			let: {
+				localFollowerId: followerId,
+				localFollowingId: followingId,
+				localMyFavorite: true,
+			},
+			pipeline: [
+				{
+					$match: {
+						$expr: {
+							$and: [{ $eq: ['$followerId', '$$localFollowerId'] }, { $eq: ['$followingId', '$$localFollowingId']}],
+						},
+					},
+				},
+				{
+					$project: {
+						_id: 0,
+						followerId: 1,
+						followingId: 1,
+						myFollowing: '$$localMyFavorite',
+					}
+				}
+			],
+			as: 'meFollowed',
+		},
+	};
+};
+
+
 export const shapeIntoMongoObjectId = (target: any) => {
 	return typeof target === 'string' ? new ObjectId(target) : target;
 };
