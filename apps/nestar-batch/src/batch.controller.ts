5b2dd7bd357c7951a2d07472b9a4a596eed920a9
IsmoilovMax <maxmudjon2121@gmail.com>
2024-09-13 06:27:09 +0900

    feat: Create batchRollback, batchPropties & batchAgents jobSchedule

--- apps/nestar-batch/src/batch.controller.ts
+++ apps/nestar-batch/src/batch.controller.ts
@@ -1,12 +1,57 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { BatchService } from './batch.service';
+import { Cron, Interval, Timeout } from '@nestjs/schedule';
 
 @Controller()
 export class BatchController {
-  constructor(private readonly nestarBatchService: BatchService) {}
+	private logger: Logger = new Logger('BatchController');
 
-  @Get()
-  getHello(): string {
-    return this.nestarBatchService.getHello();
-  }
+	constructor(private readonly batchService: BatchService) {}
+
+	@Timeout(1000)
+	handleTimeout() {
+		this.logger.debug('BATCH SERVER READY!');
+	}
+
+	@Cron('00 *  * * * *', { name: 'BATCH_ROLLBACK' })
+	public async batchRollback() {
+		try {
+			this.logger['context'] = 'BATCH_ROLLBACK';
+			this.logger.debug('EXECUTED!');
+			await this.batchService.batchRollback();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+
+	@Cron('20 *  * * * *', { name: 'BATCH_TOP_PROPERTIES' })
+	public async batchProperties() {
+		try {
+			this.logger['context'] = 'BATCH_TOP_PROPERTIES';
+			this.logger.debug('EXECUTED!');
+			await this.batchService.batchProperties();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+
+	@Cron('40 *  * * * *', { name: 'BATCH_TOP_AGENTS' })
+	public async batchAgents() {
+		try {
+			this.logger['context'] = 'BATCH_TOP_AGENTS';
+			this.logger.debug('EXECUTED!');
+			await this.batchService.batchAgents();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+	// @Interval(1000)
+	// handleInterval(){
+	//   this.logger.debug('INTERVAL TEST')
+	// }
+
+	@Get()
+	getHello(): string {
+		return this.batchService.getHello();
+	}
 }
--- apps/nestar-batch/src/batch.service.ts
+++ apps/nestar-batch/src/batch.service.ts
@@ -5,4 +5,16 @@ export class BatchService {
   getHello(): string {
     return 'Welcome to Nestart API BATCH Server';
   }
+
+  public async batchRollback(): Promise<void>{
+    console.log('batchRollback')
+  }
+  
+  public async batchProperties(): Promise<void>{
+    console.log('batchProperties')
+  }
+
+  public async batchAgents(): Promise<void>{
+    console.log('batchAgents')
+  }
 }
